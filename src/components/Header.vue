26bd71c5709ad1dbdb09512b9662afb93e8a9f73
adibemohamed <mr.eladib.mohamed@gmail.com>
2021-10-01 18:59:06 +0100

    chore: add tailwindcss

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+ <div class="p-4 bg-white">HEADER</div>
+
+  <img alt="Vue logo" src="../assets/logo.png" />
+</template>
+
+<style scoped>
+
+</style>
